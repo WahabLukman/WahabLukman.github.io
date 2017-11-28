808121840fd6c19002cc37ad785bdaf3323e81f8
WahabLukman <wahab22393@gmail.com>
2017-11-28 11:28:43 +1100

    update flickr API

--- Resources/script.js
+++ Resources/script.js
@@ -1,5 +1,5 @@
 //vue.js
-var url = 'https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=7a019945af7aaa0a3637f421454bce15&user_id=35402543%40N04&format=json&nojsoncallback=1&auth_token=72157688933765221-1c775ac7ddbc78ec&api_sig=7f36527a2e619e49d226d2843924e3e7';
+var url = 'https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=47510a816a4dcdf3de88893c6d937f27&user_id=35402543%40N04&format=json&nojsoncallback=1&auth_token=72157663079843208-e1a11d449c6a7e33&api_sig=411b99856d62c860d2951f3dbc6819b3';
 
 var app = new Vue({
   el: '#main-container',
