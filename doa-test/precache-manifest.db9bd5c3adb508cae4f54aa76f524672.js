1c15284af998534e00fb117ebde03667bee5ab71
WahabLukman <wahab22393@gmail.com>
2018-11-08 11:54:09 +1100

    test doa env deployment

---
+++ doa-test/precache-manifest.db9bd5c3adb508cae4f54aa76f524672.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ad5e947f3e9c54ee4295ff000fdf56f",
+    "url": "/static/media/picture1.5ad5e947.jpg"
+  },
+  {
+    "revision": "1a40b50cf7a6f0f24cd26c554d991c53",
+    "url": "/static/media/logo.1a40b50c.png"
+  },
+  {
+    "revision": "a6dc40691cb7e5de35c1719a7316d62e",
+    "url": "/static/media/background.a6dc4069.jpg"
+  },
+  {
+    "revision": "f487f12159c5df882653ca5f2122d169",
+    "url": "/static/media/about-image.f487f121.jpg"
+  },
+  {
+    "revision": "5ad5e947f3e9c54ee4295ff000fdf56f",
+    "url": "/static/media/about-image-2.5ad5e947.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "73fc519fc4c73d77211e",
+    "url": "/static/js/main.73fc519f.chunk.js"
+  },
+  {
+    "revision": "8e2bbc5c0552172f1005",
+    "url": "/static/js/1.8e2bbc5c.chunk.js"
+  },
+  {
+    "revision": "73fc519fc4c73d77211e",
+    "url": "/static/css/main.5cf6a5df.chunk.css"
+  },
+  {
+    "revision": "d802c41f2cae1fd42162f1e8491af762",
+    "url": "/index.html"
+  }
+];
